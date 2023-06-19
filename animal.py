47f9e2e21ad4a35aec9ae4b8fc41f1c19cc01868
CariocaFellipe <87872775+CariocaFellipe@users.noreply.github.com>
2023-06-19 11:29:42 -0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,11 +1,13 @@
 class Animal:
-    def __init__(self, tipo, idade, cor):
+    def __init__(self, tipo, idade, cor, porte):
         self.tipo = tipo
         self.idade = idade
         self.cor = cor
+        self.porte = porte
 
     def exibir_detalhes(self):
         print(f"Tipo: {self.tipo}")
         print(f"Idade: {self.idade}")
         print(f"Cor: {self.cor}")
+        print(f"Porte: {self.porte}")
         print()
