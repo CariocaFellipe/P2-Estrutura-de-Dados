cee3cd1d395128c4d976464313d5bd19e4970ee1
CariocaFellipe <87872775+CariocaFellipe@users.noreply.github.com>
2023-06-19 11:36:11 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,10 +1,11 @@
 class Pessoa:
-    def __init__(self, nome, contato, especie, idade, cor):
+    def __init__(self, nome, contato, especie, idade, cor, porte):
         self.nome = nome
         self.contato = contato
         self.especie = especie
         self.idade = idade
         self.cor = cor
+        self.porte = porte
 
     def verificar_compatibilidade(self, animal):
         return (
